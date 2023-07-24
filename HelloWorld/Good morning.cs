851d38da15d9742ed0b9ea0be5e0342965db80d0
maxiimize <max.b@hotmail.se>
2023-07-25 00:44:01 +0200

    Add project files.

---
+++ HelloWorld/Good morning.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
+
+namespace goodMorning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = 8;
+            if (time >= 20)
+            {
+                Console.WriteLine("Good evening.");
+                }
+            else if (time >= 12)
+            {
+                Console.WriteLine("Good day.");
+            }
+            else
+            {
+                Console.WriteLine("Good morning.");
+            }
+        }
+        }
+                
+    }    
